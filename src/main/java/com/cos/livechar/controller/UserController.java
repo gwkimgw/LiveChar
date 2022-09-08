afa8f37dde63d07a13f6b830d9c6df1138b1cd96
gwkimgw <marianaaquaria@gmail.com>
2022-09-08 14:04:39 +0900

    feat: Add annotation Add CrossOrigin

--- src/main/java/com/cos/livechar/controller/UserController.java
+++ src/main/java/com/cos/livechar/controller/UserController.java
@@ -2,6 +2,7 @@ package com.cos.livechar.controller;
 
 import com.cos.livechar.storage.UserStorage;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Set;
 
 @RestController
+@CrossOrigin
 public class UserController {
     @GetMapping("/registration/{userName}")
     public ResponseEntity<Void> register(@PathVariable String userName) {
